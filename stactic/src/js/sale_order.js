cc37dadb4a1bc2a2ab2a790bd59b523931cde05a
Angel Barrera <144848292+TheAng3ll@users.noreply.github.com>
2024-06-20 13:30:54 -0600

    Update sale_order.js

--- stactic/src/js/sale_order.js
+++ stactic/src/js/sale_order.js
@@ -6,7 +6,7 @@ odoo.define('Kingstrong_extended', function (require) {
     FormView.include({
         render_field: function (field_name) {
             this._super.apply(this, arguments);
-            if (field_name === 'document_origin') {
+            if (field_name === 'origin') {
                 this.$el.find('input[name="origin"]').attr('disabled', 'disabled').css('opacity', '0.5');
             }
         },
