108b198b0b51bcef8d85ad87b5445de4e2232122
Angel Barrera <144848292+TheAng3ll@users.noreply.github.com>
2024-08-09 11:47:08 -0600

    Update contactos_telefono.py
    CRM

--- models/contactos_telefono.py
+++ models/contactos_telefono.py
@@ -37,3 +37,34 @@ class Contactos(models.Model):
                 if existing_phone:  # Verificar duplicados al encontrar que si tenga valor
                     raise ValidationError("El número de teléfono ya está registrado.")
 
+
+
+class crmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    def format_phone_number(self, number): # Formatear el número en el formato +52 1 XX XXXX XXXX
+        return '+52 1 {} {} {}'.format(number[:2], number[2:6], number[6:])
+
+
+    @api.onchange('phone') #funcion para verificar el formato del numero de telefono, digitos, cantidad y solo acepta numeros
+    def _onchange_phone(self):
+        if self.phone:
+            clean_number = re.sub(r'\D', '', self.phone) #Eliminar los caracteres no numericos
+            if len(clean_number) == 10: # solo acepta 10 digitos una vez limpio
+                formatted_number = self.format_phone_number(clean_number) #la variable adquiere el valor de el numero con el formato
+                self.phone = formatted_number # Imprime el numero en el campo front
+            else:
+                raise ValidationError("Numero invalido: solo se permiten numeros de 10 digitos sin incluir lada.")
+        else: #si el campo esta vacio imprime este defecto
+            self.phone = '+52 1 00 0000 0000'
+
+    @api.constrains('phone')  
+    def _check_phone(self):  # Función para comparar los campos de teléfono 
+        for record in self:  # Iterar sobre todos los registros
+            if record.phone and record.phone != '+52 1 00 0000 0000':  # Validar formato y que sea diferen al de defecto
+                existing_phone = self.env['res.partner'].search([('phone', '=', record.phone), ('id', '!=', record.id)]) #al ecnontrar le da valor
+
+                if existing_phone:  # Verificar duplicados al encontrar que si tenga valor
+                    raise ValidationError("El número de teléfono ya está registrado.")
+
+
