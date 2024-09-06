b4571e2613298d37c883ff5056cd6dd2b20e0be4
Angel Barrera <144848292+TheAng3ll@users.noreply.github.com>
2024-09-06 13:08:51 -0600

    Update contacto_nombre.py
    Elimino codigo inecesario

--- models/contacto_nombre.py
+++ models/contacto_nombre.py
@@ -31,11 +31,4 @@ class CrmLead(models.Model):
         if self.name:
             self.name = self.name.upper()
 
-    @api.constrains('name')
-    def _check_name(self): #funcion para  comparar que no exista ya un nombre
-        for record in self:
-            if not self.env.user.has_group('Kingstrong_extended.group_allow_duplicate_contact'):
-                existing_partners = self.env['res.partner'].search([('name', '=', record.name), ('id', '!=', record.id )], limit = 1) #busca el nombre
-                if existing_partners:
-                    raise ValidationError("Este usuario ya esta registrado, si crees que es un usuario nuevo porfavor contacta a tu gerente.")
 
